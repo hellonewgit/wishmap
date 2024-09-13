defa557960e7f4eb139db0438151f2ad45b55bb7
Алексей windows <alexey_kadochnikov@mail.ru>
2024-09-13 12:27:29 +0300

    добавил компонент с цитатой

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import DesireCardList from './components/DesireCartList/DesireCartList'
+import RandomQuote from './components/RandomQuote/RandomQuote'
 
 function App() {
   return (
     <>
-      <h1>Карта Желаний</h1>
-      <DesireCardList />
+    <h1>Карта Желаний</h1>
+    <RandomQuote />
+    <DesireCardList />
     </>
   )
 }
--- src/components/DesireCartList/DesireCartList.jsx
+++ src/components/DesireCartList/DesireCartList.jsx
@@ -5,25 +5,25 @@ import styles from './DesireCardList.module.css';
 const DesireCardList = () => {
     // Массив с данными для карточек
     const desires = [
-        { title: 'Деньги', imageUrl: 'https://ic.pics.livejournal.com/xachapyri/9652291/5824/5824_320.jpg' },
-        { title: 'Семья', imageUrl: 'https://ic.pics.livejournal.com/xachapyri/9652291/5824/5824_320.jpg' },
-        { title: 'Путешествия', imageUrl: 'https://ic.pics.livejournal.com/xachapyri/9652291/5824/5824_320.jpg' },
-        { title: 'Успех', imageUrl: 'https://ic.pics.livejournal.com/xachapyri/9652291/5824/5824_320.jpg' },
-        { title: 'Карьера', imageUrl: 'https://ic.pics.livejournal.com/xachapyri/9652291/5824/5824_320.jpg' },
-        { title: 'Саморазвитие', imageUrl: 'https://ic.pics.livejournal.com/xachapyri/9652291/5824/5824_320.jpg' },
+        { title: 'Деньги', imageUrl: '/src/assets/images/wishmapimg.jpg' },
+        { title: 'Семья', imageUrl: '/src/assets/images/wishmapimg.jpg' },
+        { title: 'Путешествия', imageUrl: '/src/assets/images/wishmapimg.jpg' },
+        { title: 'Успех', imageUrl: '/src/assets/images/wishmapimg.jpg' },
+        { title: 'Карьера', imageUrl: '/src/assets/images/wishmapimg.jpg' },
+        { title: 'Саморазвитие', imageUrl: '/src/assets/images/wishmapimg.jpg' },
     ];
-    
+
     return (
         <div>
-        <div className={styles['desire-card-list']}>
-        {desires.map((desire, index) => (
-            <DesireCard 
-            key={index} 
-            title={desire.title} 
-            imageUrl={desire.imageUrl} 
-            />
-        ))}
-        </div>
+            <div className={styles['desire-card-list']}>
+                {desires.map((desire, index) => (
+                    <DesireCard
+                        key={index}
+                        title={desire.title}
+                        imageUrl={desire.imageUrl}
+                    />
+                ))}
+            </div>
         </div>
     );
 };
