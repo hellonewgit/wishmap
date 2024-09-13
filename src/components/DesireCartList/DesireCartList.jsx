544deb1c68102038e05917abfe7aecc4a4b76e00
Алексей windows <alexey_kadochnikov@mail.ru>
2024-09-13 14:33:46 +0300

    Обновил фото и адаптив

--- src/components/DesireCartList/DesireCartList.jsx
+++ src/components/DesireCartList/DesireCartList.jsx
@@ -5,16 +5,17 @@ import styles from './DesireCardList.module.css';
 const DesireCardList = () => {
     // Массив с данными для карточек
     const desires = [
-        { title: 'Деньги', imageUrl: '/wishmap/assets/images/wishmapimg.jpg' },
-        { title: 'Семья', imageUrl: '/wishmap/assets/images/wishmapimg.jpg' },
-        { title: 'Путешествия', imageUrl: '/wishmap/assets/images/wishmapimg.jpg' },
-        { title: 'Успех', imageUrl: '/wishmap/assets/images/wishmapimg.jpg' },
-        { title: 'Карьера', imageUrl: '/wishmap/assets/images/wishmapimg.jpg' },
-        { title: 'Саморазвитие', imageUrl: '/wishmap/assets/images/wishmapimg.jpg' },
+        { title: 'Деньги', imageUrl: '/wishmap/assets/images/money.jpg' },
+        { title: 'Семья', imageUrl: '/wishmap/assets/images/family.jpg' },
+        { title: 'Путешествия', imageUrl: '/wishmap/assets/images/travel.jpg' },
+        { title: 'Успех', imageUrl: '/wishmap/assets/images/success.jpg' },
+        { title: 'Карьера', imageUrl: '/wishmap/assets/images/carier.jpg' },
+        { title: 'Саморазвитие', imageUrl: '/wishmap/assets/images/self.jpg' },
     ];
 
     return (
         <div>
+            <h2>Выбирай что хочешь</h2>
             <div className={styles['desire-card-list']}>
                 {desires.map((desire, index) => (
                     <DesireCard
