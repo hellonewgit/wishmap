c2dc019c4bc57470c18c4160e22fcf60ddc79e84
Алексей windows <alexey_kadochnikov@mail.ru>
2024-09-15 22:31:19 +0300

    Восстановил актуальные изменения и добавил корректировки

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DesireCardList from './components/DesireCartList/DesireCartList';
 import RandomQuote from './components/RandomQuote/RandomQuote';
-import RegistrationButton from './components/RegistrationButton/RegistrationButton';
-
+import RegisterPage from './pages/RegisterPage/RegisterPage';
+import Header from './components/Header/Header';  // Импортируем компонент шапки
 
 function App() {
   return (
-    <>
-      <header>
-        <RegistrationButton />
-      </header>
-      <h1>Карта Желаний</h1>
-      <RandomQuote />
-      <DesireCardList />
-    </>
+    <Router basename="/wishmap">
+      <Header />  {/* Используем компонент Header */}
+      <div className="center">  {/* Добавляем класс center */}
+        <Routes>
+          <Route path="/" element={(
+            <>
+              <h1>Карта Желаний</h1>
+              <RandomQuote />
+              <DesireCardList />
+            </>
+          )} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
